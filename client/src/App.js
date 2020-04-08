c6913f631b561cf86f6bd244994435954144f4ac
Jean Ribac <jeanribac95@gmail.com>
2020-04-08 15:54:40 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore db

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,37 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sing-up.component';
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument
+} from './firebase/firebase.utils';
 
 class App extends Component {
   constructor(props) {
     super(props);
     
-    this.state = {curentUser: null}
+    this.state = {currentUser: null}
   }
   
   UnsubscribeFromAuth = null;
   componentDidMount(){
-    this.UnsubscribeFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({curentUser: user});
-      console.log(user);
+    this.UnsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot =>{
+          // console.log(snapShot.data())
+           this.setState({
+             currentUser: {
+               id:snapShot.id,
+               ...snapShot.data()
+             }
+           },()=>{
+            console.log(this.state)
+           });  
+        });   
+      }else{
+        this.setState({currentUser:userAuth})
+      }
     })
   }
 
--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -2,25 +2,44 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDVrBsyeYFLoUqWjgKApqyjtiNBWAbZ_nw",
-    authDomain: "e-commercereactwebsite.firebaseapp.com",
-    databaseURL: "https://e-commercereactwebsite.firebaseio.com",
-    projectId: "e-commercereactwebsite",
-    storageBucket: "e-commercereactwebsite.appspot.com",
-    messagingSenderId: "552739498652",
-    appId: "1:552739498652:web:493b10be7cf66770775b59",
-    measurementId: "G-H9V6J4WPJS"
+var config = {
+  apiKey: "AIzaSyDVrBsyeYFLoUqWjgKApqyjtiNBWAbZ_nw",
+  authDomain: "e-commercereactwebsite.firebaseapp.com",
+  databaseURL: "https://e-commercereactwebsite.firebaseio.com",
+  projectId: "e-commercereactwebsite",
+  storageBucket: "e-commercereactwebsite.appspot.com",
+  messagingSenderId: "552739498652",
+  appId: "1:552739498652:web:493b10be7cf66770775b59",
+  measurementId: "G-H9V6J4WPJS"
 };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+export const createUserProfileDocument = async(userAuth, additionalData) =>{
+  if(!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:"select_account"});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  if(!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    
+    try{
+      await userRef.set({displayName, email, createdAt, ...additionalData})
+    }catch(err){
+      console.log("error creating user", err.message)
+    }
+  }
+  return userRef;
+}
 
-  export default firebase;
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:"select_account"});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
